e69ccb3f926b48514dbc637f52989e496bca079e
Oscar-FV <oscar.flores.vqz@gmail.com>
2025-12-20 23:58:07 -0600

    ft: custom navbar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,64 +1,81 @@
 import { Button } from '@/components/ui/button';
 import { Icon } from '@/components/ui/icon';
-import { NAV_THEME } from '@/lib/theme';
-import { Tabs } from 'expo-router';
-import { HomeIcon, MoonStarIcon, SunIcon, TelescopeIcon } from 'lucide-react-native';
-import { useColorScheme } from 'nativewind';
+import { Text } from '@/components/ui/text';
+import { StyleSheet } from 'react-native';
+import { Tabs, TabList, TabTrigger, TabSlot } from 'expo-router/ui';
 import * as React from 'react';
+import { View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import { LinearGradient } from 'expo-linear-gradient';
+import { THEME } from '@/lib/theme';
+import { useColorScheme } from 'nativewind';
+import { House, Search, User } from 'lucide-react-native';
+import NavButton from '@/components/nav-button';
 
 export default function TabLayout() {
+  const { bottom } = useSafeAreaInsets();
   const { colorScheme } = useColorScheme();
-  const theme = NAV_THEME[colorScheme ?? 'light'];
 
   return (
-    <Tabs
-      screenOptions={{
-        headerRight: () => <ThemeToggle />,
-        tabBarActiveTintColor: theme.colors.primary,
-        headerTitleAlign: 'center',
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <TabBarIcon as={HomeIcon} color={color} />,
-        }}
+    <Tabs className="relative">
+      <TabSlot />
+      <LinearGradient
+        colors={[
+          THEME[colorScheme ?? 'light'].backgroundTransparent,
+          THEME[colorScheme ?? 'light'].backgroundTransparent2,
+          THEME[colorScheme ?? 'light'].background,
+        ]}
+        locations={[0, 0.3, 1]}
+        start={{ x: 0, y: 0 }}
+        end={{ x: 0, y: 1 }}
+        style={styles.background}
+        pointerEvents="none"
       />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <TabBarIcon as={TelescopeIcon} color={color} />,
-        }}
-      />
+      <TabList
+        style={[
+          styles.tabList,
+          {
+            gap: 16,
+            paddingBottom: bottom,
+            paddingHorizontal: 42,
+          },
+        ]}>
+        <TabTrigger name="index" href="/(tabs)" asChild>
+          <NavButton icon={House} />
+        </TabTrigger>
+
+        <TabTrigger name="explore" href="/explore" asChild>
+          <NavButton buttonText="Search" icon={Search} className="grow" />
+        </TabTrigger>
+
+        <TabTrigger name="profile" href="/profile" asChild>
+          <NavButton icon={User} />
+        </TabTrigger>
+      </TabList>
     </Tabs>
   );
 }
 
-type TabBarIconProps = {
-  as: React.ComponentProps<typeof Icon>['as'];
-  color: string;
-};
-
-function TabBarIcon({ as, color }: TabBarIconProps) {
-  return <Icon as={as} color={color} size={20} />;
-}
-
-const THEME_ICONS = {
-  light: SunIcon,
-  dark: MoonStarIcon,
-};
-
-function ThemeToggle() {
-  const { colorScheme, toggleColorScheme } = useColorScheme();
-
-  return (
-    <Button
-      onPressIn={toggleColorScheme}
-      size="icon"
-      variant="ghost"
-      className="ios:size-9 rounded-full web:mx-4">
-      <Icon as={THEME_ICONS[colorScheme ?? 'light']} className="size-5" />
-    </Button>
-  );
-}
+const styles = StyleSheet.create({
+  tabList: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'transparent',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+  background: {
+    position: 'absolute',
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: 60,
+  },
+  tabTriggerItem: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ components/nav-button.tsx
@@ -0,0 +1,70 @@
+import { LucideProps } from 'lucide-react-native';
+import React, { ForwardRefExoticComponent } from 'react';
+import { Pressable, PressableProps } from 'react-native';
+import { Icon } from './ui/icon';
+import { Text } from './ui/text';
+
+export interface NavButtonProps extends PressableProps {
+  icon: ForwardRefExoticComponent<LucideProps>;
+  buttonText?: string;
+  isFocused?: boolean;
+  className?: string;
+}
+
+export default function NavButton({
+  icon,
+  buttonText,
+  className = '',
+  isFocused,
+  ...props
+}: NavButtonProps) {
+
+  return (
+    <Pressable
+      {...props}
+      accessibilityRole="button"
+      className={[
+        style.container,
+        isFocused ? style.containerFocused : style.containerIdle,
+        className,
+      ].join(' ')}
+    >
+      <Icon
+        as={icon}
+        className={[
+          style.icon,
+          isFocused ? style.iconFocused : style.iconIdle,
+        ].join(' ')}
+      />
+
+      {buttonText && (
+        <Text
+          className={[
+            style.text,
+            isFocused ? style.textFocused : style.textIdle,
+          ].join(' ')}
+        >
+          {buttonText}
+        </Text>
+      )}
+    </Pressable>
+  );
+}
+
+const style = {
+  // Base
+  container:
+    'group shrink-0 flex-row items-center !justify-center gap-4 rounded-full p-3',
+  icon: 'w-5 h-5',
+  text: 'text-sm font-medium',
+
+  // States
+  containerFocused: 'bg-primary  shadow-md shadow-primary/70',
+  containerIdle: 'bg-background shadow-sm shadow-black/20',
+
+  iconFocused: 'text-primary-foreground',
+  iconIdle: 'text-muted-foreground',
+
+  textFocused: 'text-primary-foreground',
+  textIdle: 'text-muted-foreground',
+};
